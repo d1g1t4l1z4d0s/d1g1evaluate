93b2bd328d5215dc49f481a1333e6fe579f5ad3d
d1g1t4l1z4d0s <d1g1t4l1z4d0s.pyp@gmail.com>
2023-08-17 13:01:23 -0500

    Deploy: config yml

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'https://github.com/d1g1t4l1z4d0s/d1g1evaluate'
 })
