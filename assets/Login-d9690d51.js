e827a657a2437ec444a7304ecb1ff2237edc8bcc
d1g1t4l1z4d0s <d1g1t4l1z4d0s.pyp@gmail.com>
2023-08-18 11:47:44 -0500

    Updates

---
+++ assets/Login-d9690d51.js
@@ -0,0 +1,1 @@
+import{c as s,j as a,a as o}from"./index-7d4ac418.js";import{U as e}from"./UserForm-b885fbf0.js";function i(){const{mutate:n}=s();return a("main",{className:"content",children:[o("h1",{children:"Login"}),o(e,{mode:"login",mutate:n})]})}export{i as default};
